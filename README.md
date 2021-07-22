d4ce27d8fcce59240213e0183d477005eef32c56
haechan07 <limhaechan0711@gmail.com>
2021-07-23 02:12:39 +0900

    English Support

--- README.md
+++ README.md
@@ -1,23 +1,50 @@
-# 내신 등급 계산기 1.2
+# 내신 등급 계산기 1.3
 
 과목명, 수강자 수, 석차, 동석차 수만 입력하면 해당하는 등급을 알려드립니다!
 잘못된 계산 결과가 나온경우 깃허브에 이슈를 올려주세요!
 
 ## 사용법
-과목명, 수강자 수, 석차, 동석차 수를 반복적으로 입력하셔야 합니다.
+과목명, 수강자 수, 석차, 동석차 수를 반복적으로 입력합니다.
 
 > 과목명을(를) 입력해주세요 : 영어
-> 수강자 수을(를) 입력해주세요 : 299
-> 석차을(를) 입력해주세요 : 43
-> 동석차 수을(를) 입력해주세요 : 0
+> 수강자 수을(를) 입력해주세요 : 293
+> 석차을(를) 입력해주세요 : 10
+> 동석차 수을(를) 입력해주세요 : 2
 > 멈추려면 Y를 입력해주세요 : Y
 
 위와 같이 입력할 경우 아래와 같이 출력됩니다.
 
-> 영어 : 3등급
+> 영어 : 1등급
 
-그 후 프로그램의 종료를 막기위해 입력을 받습니다.
+그 후 프로그램의 종료를 막기위해 입력을 요청합니다.
 
 > Enter 키를 눌러주세요.
 
-이제 강제로 프로그램을 종료하거나 Enter를 누르면 종료됩니다.
+이제 수동으로 프로그램을 종료하거나 Enter를 누르면 종료됩니다.
+
+------
+
+# Korean High School Grade Calculator 1.3
+
+Just input Subject's Name, Listeners, Rank, The number of Same Rank, Then It'll let you know your grade!
+If wrong result was printed, Make an Issue in This repository!
+
+## How to use
+
+Input Subject's Name, The number of Examiners, Rank, The number of Same Rank repeatedly
+
+> Please Input Subject : English
+> Please Input The number of Examinees : 293
+> Please Input Rank : 10
+> Please Input The number of Same Rank : 2
+> Input Y to Stop : Y
+
+If you input like example, It will print like this.(Due to My Programming bad skill, there is grammertical Error.)
+
+> English : 1th Grade
+
+Then, It request you to input in order to stop exitting Program.
+
+> Press Enter to quit 
+
+Now, You can Press Enter or manually shut it off to quit.
--- src/calcEng.kt
+++ src/calcEng.kt
@@ -6,10 +6,10 @@ fun main() {
     var toStop = false // 반복문 중단 불린
     val sb = StringBuilder() // 스트링 빌더 선언
     while (!toStop) { // 반복문
-        val className = getThing("과목명") // 과목명 입력
-        val learners = getThing("수강자 수").toInt() // 수강자 수 입력
-        var placement = getThing("석차").toDouble() // 석차 입력
-        val samePlaces = getThing("동석차 수").toInt() // 동석차 수 입력
+        val className = getThing("Subject") // 과목명 입력
+        val learners = getThing("The number of Examinees").toInt() // 수강자 수 입력
+        var placement = getThing("Rank").toDouble() // 석차 입력
+        val samePlaces = getThing("The number of Same Rank").toInt() // 동석차 수 입력
         when (samePlaces != 0) {
             true -> placement += (samePlaces - 1).toDouble() / 2 // 동석차 수가 0이 아닐 경우 중간 순위 계산 공식 사용
         }
@@ -30,23 +30,23 @@ fun main() {
             else -> 9 // 9등급
         }
         list.add(Pair(className, gradeClass)) // 과목명과 등급을 Pair 자료형을 리스트에 추가한다.
-        print("멈추려면 Y를 입력해주세요 : ")
+        print("Input Y to Stop : ")
         when (readLine()!! == "Y") {
             true -> toStop = true // "Y"가 입력되면 toStop 변수를 true로 바꾸어 반복을 멈춘다.
         }
     } // 반복문 종료
     for (i in list.indices) { // 리스트 전체
-        sb.append("${list[i].first} : ${list[i].second}등급") // "{과목명} : {등급 변수}등급" 스트링 빌더에 추가
+        sb.append("${list[i].first} : ${list[i].second}th Grade") // "{과목명} : {등급 변수}등급" 스트링 빌더에 추가
         sb.append("\n") // 줄바꿈
     }
     print("\n") // 줄바꿈
     println(sb) // 스트링 빌더 출력
-    print("Enter 키를 눌러주세요. ")
+    print("Press Enter to quit ")
     readLine() // 프로그램 중단을 막음
 }
 
 private fun getThing(input : String) : String { // 입력 함수, 파라미터 input을 넣어 출력 후 입력받은 값을 리턴한다.
-    print("${input}을(를) 입력해주세요 : ") // 파라미터 input을 넣어 출력
+    print("Please Input ${input} : ") // 파라미터 input을 넣어 출력
     val thing = readLine()!! // 입력받기
     when (thing) {
         "" -> getThing(input) // 입력받은 값이 ""일 경우 다시 한다.
